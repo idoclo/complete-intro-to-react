72a5b017653e357f658ec89c9d33a423772ac7e3
Preda Anuvatnujotikul <preda.anu@gmail.com>
2017-08-20 12:27:36 -0700

    On DATA module - URL ...saving progress

--- js/Details.jsx
+++ js/Details.jsx
@@ -2,10 +2,14 @@
 
 import React from 'react';
 
-const Details = (props) => (
-  <div className="details">
-    <h1><pre><code>{JSON.stringify(props, null, 4)}</code></pre></h1>
-  </div>
-);
+const Details = function (props: {show: React.PropTypes.string.isRequired}) {
+  console.log('props in details', props);
+  return (
+    <div className="details">
+      {/* <p><pre><code>{JSON.stringify(props, null, 4)}</code></pre></p> */}
+      <h1>{JSON.stringify(props.show)}</h1>
+    </div>
+  )
+}
 
 export default Details;
