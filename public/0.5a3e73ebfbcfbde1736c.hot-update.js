729664f6a9f4f6ad37627b260f40f557455fce73
Preda Anuvatnujotikul <preda.anu@gmail.com>
2017-08-25 21:41:38 -0700

    Added further note to Hot Module Replacement plugin in .babelrc

---
+++ public/0.5a3e73ebfbcfbde1736c.hot-update.js
@@ -0,0 +1,587 @@
+webpackHotUpdate(0,{
+
+/***/ "./data.json":
+false,
+
+/***/ "./js/ClientApp.jsx":
+/***/ (function(module, __webpack_exports__) {
+
+"use strict";
+eval("throw new Error(\"Module build failed: Error: No ESLint configuration found.\\n    at getLocalConfig (/Users/predaanuvatnujotikul/Documents/Careers/Frontend Masters/complete-intro-to-react/node_modules/eslint/lib/config.js:162:35)\\n    at Config.getConfig (/Users/predaanuvatnujotikul/Documents/Careers/Frontend Masters/complete-intro-to-react/node_modules/eslint/lib/config.js:260:26)\\n    at processText (/Users/predaanuvatnujotikul/Documents/Careers/Frontend Masters/complete-intro-to-react/node_modules/eslint/lib/cli-engine.js:224:33)\\n    at CLIEngine.executeOnText (/Users/predaanuvatnujotikul/Documents/Careers/Frontend Masters/complete-intro-to-react/node_modules/eslint/lib/cli-engine.js:754:26)\\n    at lint (/Users/predaanuvatnujotikul/Documents/Careers/Frontend Masters/complete-intro-to-react/node_modules/eslint-loader/index.js:211:17)\\n    at Object.module.exports (/Users/predaanuvatnujotikul/Documents/Careers/Frontend Masters/complete-intro-to-react/node_modules/eslint-loader/index.js:206:21)\");//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9qcy9DbGllbnRBcHAuanN4LmpzIiwic291cmNlcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlUm9vdCI6IiJ9");
+
+/***/ }),
+
+/***/ "./js/Landing.jsx":
+false,
+
+/***/ "./js/Search.jsx":
+false,
+
+/***/ "./js/ShowCard.jsx":
+false,
+
+/***/ "./node_modules/fbjs/lib/EventListener.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/ExecutionEnvironment.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/camelize.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/camelizeStyleName.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/containsNode.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/createArrayFromMixed.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/createNodesFromMarkup.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/focusNode.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/getActiveElement.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/getMarkupWrap.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/getUnboundedScrollPosition.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/hyphenate.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/hyphenateStyleName.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/isNode.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/isTextNode.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/memoizeStringOnly.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/performance.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/performanceNow.js":
+false,
+
+/***/ "./node_modules/fbjs/lib/shallowEqual.js":
+false,
+
+/***/ "./node_modules/history/DOMUtils.js":
+false,
+
+/***/ "./node_modules/history/LocationUtils.js":
+false,
+
+/***/ "./node_modules/history/PathUtils.js":
+false,
+
+/***/ "./node_modules/history/createBrowserHistory.js":
+false,
+
+/***/ "./node_modules/history/createHashHistory.js":
+false,
+
+/***/ "./node_modules/history/createMemoryHistory.js":
+false,
+
+/***/ "./node_modules/history/createTransitionManager.js":
+false,
+
+/***/ "./node_modules/hoist-non-react-statics/index.js":
+false,
+
+/***/ "./node_modules/invariant/browser.js":
+false,
+
+/***/ "./node_modules/prop-types/factoryWithThrowingShims.js":
+false,
+
+/***/ "./node_modules/prop-types/index.js":
+false,
+
+/***/ "./node_modules/react-dom/index.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ARIADOMPropertyConfig.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/AutoFocusUtils.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/BeforeInputEventPlugin.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/CSSProperty.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/CSSPropertyOperations.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/CallbackQueue.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ChangeEventPlugin.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/DOMChildrenOperations.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/DOMLazyTree.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/DOMNamespaces.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/DOMProperty.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/DOMPropertyOperations.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/Danger.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/DefaultEventPluginOrder.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/EnterLeaveEventPlugin.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/EventPluginHub.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/EventPluginRegistry.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/EventPluginUtils.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/EventPropagators.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/FallbackCompositionState.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/HTMLDOMPropertyConfig.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/KeyEscapeUtils.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/LinkedValueUtils.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/PooledClass.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactBrowserEventEmitter.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactChildReconciler.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactComponentBrowserEnvironment.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactComponentEnvironment.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactCompositeComponent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOM.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMComponent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMComponentFlags.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMComponentTree.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMContainerInfo.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMEmptyComponent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMFeatureFlags.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMIDOperations.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMInput.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMInvalidARIAHook.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMNullInputValuePropHook.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMOption.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMSelect.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMSelection.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMTextComponent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMTextarea.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMTreeTraversal.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDOMUnknownPropertyHook.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDebugTool.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDefaultBatchingStrategy.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactDefaultInjection.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactElementSymbol.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactEmptyComponent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactErrorUtils.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactEventEmitterMixin.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactEventListener.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactFeatureFlags.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactHostComponent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactHostOperationHistoryHook.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactInjection.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactInputSelection.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactInstanceMap.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactInstrumentation.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactInvalidSetStateWarningHook.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactMarkupChecksum.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactMount.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactMultiChild.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactNodeTypes.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactOwner.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactPropTypeLocationNames.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactPropTypesSecret.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactReconcileTransaction.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactReconciler.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactRef.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactServerRenderingTransaction.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactServerUpdateQueue.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactUpdateQueue.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactUpdates.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ReactVersion.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SVGDOMPropertyConfig.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SelectEventPlugin.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SimpleEventPlugin.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SyntheticAnimationEvent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SyntheticClipboardEvent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SyntheticCompositionEvent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SyntheticDragEvent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SyntheticEvent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SyntheticFocusEvent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SyntheticInputEvent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SyntheticKeyboardEvent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SyntheticMouseEvent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SyntheticTouchEvent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SyntheticTransitionEvent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SyntheticUIEvent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/SyntheticWheelEvent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/Transaction.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/ViewportMetrics.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/accumulateInto.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/adler32.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/checkReactTypeSpec.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/createMicrosoftUnsafeLocalFunction.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/dangerousStyleValue.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/escapeTextContentForBrowser.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/findDOMNode.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/flattenChildren.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/forEachAccumulated.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/getEventCharCode.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/getEventKey.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/getEventModifierState.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/getEventTarget.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/getHostComponentFromComposite.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/getIteratorFn.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/getNodeForCharacterOffset.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/getTextContentAccessor.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/getVendorPrefixedEventName.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/instantiateReactComponent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/isEventSupported.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/isTextInputElement.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/quoteAttributeValueForBrowser.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/reactProdInvariant.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/renderSubtreeIntoContainer.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/setInnerHTML.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/setTextContent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/shouldUpdateReactComponent.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/traverseAllChildren.js":
+false,
+
+/***/ "./node_modules/react-dom/lib/validateDOMNesting.js":
+false,
+
+/***/ "./node_modules/react-router-dom/es/BrowserRouter.js":
+false,
+
+/***/ "./node_modules/react-router-dom/es/HashRouter.js":
+false,
+
+/***/ "./node_modules/react-router-dom/es/Link.js":
+false,
+
+/***/ "./node_modules/react-router-dom/es/MemoryRouter.js":
+false,
+
+/***/ "./node_modules/react-router-dom/es/NavLink.js":
+false,
+
+/***/ "./node_modules/react-router-dom/es/Prompt.js":
+false,
+
+/***/ "./node_modules/react-router-dom/es/Redirect.js":
+false,
+
+/***/ "./node_modules/react-router-dom/es/Route.js":
+false,
+
+/***/ "./node_modules/react-router-dom/es/Router.js":
+false,
+
+/***/ "./node_modules/react-router-dom/es/StaticRouter.js":
+false,
+
+/***/ "./node_modules/react-router-dom/es/Switch.js":
+false,
+
+/***/ "./node_modules/react-router-dom/es/index.js":
+false,
+
+/***/ "./node_modules/react-router-dom/es/matchPath.js":
+false,
+
+/***/ "./node_modules/react-router-dom/es/withRouter.js":
+false,
+
+/***/ "./node_modules/react-router/es/MemoryRouter.js":
+false,
+
+/***/ "./node_modules/react-router/es/Prompt.js":
+false,
+
+/***/ "./node_modules/react-router/es/Redirect.js":
+false,
+
+/***/ "./node_modules/react-router/es/Route.js":
+false,
+
+/***/ "./node_modules/react-router/es/Router.js":
+false,
+
+/***/ "./node_modules/react-router/es/StaticRouter.js":
+false,
+
+/***/ "./node_modules/react-router/es/Switch.js":
+false,
+
+/***/ "./node_modules/react-router/es/index.js":
+false,
+
+/***/ "./node_modules/react-router/es/matchPath.js":
+false,
+
+/***/ "./node_modules/react-router/es/withRouter.js":
+false,
+
+/***/ "./node_modules/react-router/node_modules/isarray/index.js":
+false,
+
+/***/ "./node_modules/react-router/node_modules/path-to-regexp/index.js":
+false,
+
+/***/ "./node_modules/react/lib/getNextDebugID.js":
+false,
+
+/***/ "./node_modules/resolve-pathname/index.js":
+false,
+
+/***/ "./node_modules/styled-components/dist/styled-components.es.js":
+false,
+
+/***/ "./node_modules/stylis/stylis.js":
+false,
+
+/***/ "./node_modules/value-equal/index.js":
+false,
+
+/***/ "./node_modules/warning/browser.js":
+false
+
+})
